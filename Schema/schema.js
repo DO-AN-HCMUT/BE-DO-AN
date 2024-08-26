961cb29a29873c2a81f933afeb01c14393088c1b
vwaizer <lightwing2208@gmail.com>
2024-08-26 09:18:09 +0700

    add schema

--- Schema/schema.js
+++ Schema/schema.js
@@ -9,3 +9,30 @@ export class User {
       
   }
 }
+export class Chat{
+  constructor(chat){
+    this.userIDs=chat.userIDs || [],
+    this.message=chat.message || [],
+    // message struct {userID:id,content:" "}
+    this.createdDate= new Date()
+  }
+}
+export class Project{
+  constructor(project){
+    this.leaderID=project.leaderID,
+    this.members=project.members || [],
+    this.chatIDs=project.chatIDs || [],
+    this.createdDate=new Date(),
+    this.taskIDs=project.taskIDs || []
+  }
+}
+export class Task{
+  constructor(task){
+    this.projectID=task.projectID,
+    this.registeredMembers=task.registeredMembers || [],
+    this.description=task.description || "",
+    this.status=task.status || "new",
+    this.endDate=task.endDate,
+    this.createdDate=new Date()
+  }
+}
