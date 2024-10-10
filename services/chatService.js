c2336cdc8210d85a9c4bb0c637c643454aa9b33d
vwaizer <lightwing2208@gmail.com>
2024-10-10 11:39:01 +0700

    update user and chat

--- services/chatService.js
+++ services/chatService.js
@@ -4,12 +4,12 @@ import { Chat } from "../Schema/schema.js";
 
 export const getChat = async (req, res, next) => {
 	try {
-		const secondId = req.params.id				
-		const result = await databaseProject.chat.findOne({"userIDs": [req.userID,(secondId)]});		
+		const secondId = req.params.id
+		const result = await databaseProject.chat.findOne({ "userIDs": [req.userID, (secondId)] });
 		return res.json({
-			payload:result,
-			message:"success",
-			success:true
+			payload: result,
+			message: "success",
+			success: true
 		})
 	} catch (error) {
 		return next(error)
@@ -18,60 +18,67 @@ export const getChat = async (req, res, next) => {
 
 export const makeChat = async (req, res, next) => {
 	try {
-		const item = new Chat(req.body)
-		await databaseProject.chat.insertOne(item);
-		return res.json({
-			payload:{},
-			message:"success",
-			success:true
-		})
+		const checkExist = await databaseProject.chat.findOne({ userIDs: req.body.userIDs });
+		if (checkExist) {
+			return next('Exist Conservation')
+		}
+		else {
+			const item = new Chat(req.body)
+			await databaseProject.chat.insertOne(item);
+			return res.json({
+				payload: {},
+				message: "success",
+				success: true
+			})
+		}
+
 	} catch (error) {
 		return next(error)
 	}
 }
-export const deleteChat=async(req,res,next)=>{
+export const deleteChat = async (req, res, next) => {
 	try {
-		const secondId = req.params.id				
-		const result = await databaseProject.chat.findOne({'userIDs': [req.userID,(secondId)]});	
-		 await databaseProject.chat.deleteOne({_id:new ObjectId(result._id)})
+		const secondId = req.params.id
+		const result = await databaseProject.chat.findOne({ 'userIDs': [req.userID, (secondId)] });
+		await databaseProject.chat.deleteOne({ _id: new ObjectId(result._id) })
 		return res.json({
-			payload:{},
-			message:"success",
-			success:true
+			payload: {},
+			message: "success",
+			success: true
 		})
 	} catch (error) {
 		return next(error)
 	}
 }
-export const getReceiver=async(req,res,next)=>{
-	try {		
-		const userID=req.userID
-		
-		const data=await databaseProject.chat.find({"userIDs":{$all:[userID]}}).toArray()
-		const result=data?.map((item)=>{
-			if(item.userIDs[0] === userID){
+export const getReceiver = async (req, res, next) => {
+	try {
+		const userID = req.userID
+
+		const data = await databaseProject.chat.find({ "userIDs": { $all: [userID] } }).toArray()
+		const result = data?.map((item) => {
+			if (item.userIDs[0] === userID) {
 				return item.userIDs[1]
 			}
 			return item.userIDs[0]
 		})
 		return res.json({
-			payload:{sender: userID,receiver:result},
-			message:"success",
-			success:true
+			payload: { sender: userID, receiver: result },
+			message: "success",
+			success: true
 		})
 	} catch (error) {
 		return next(error)
 	}
 }
-export const addMessage=async(req,res,next)=>{
+export const addMessage = async (req, res, next) => {
 	try {
-		const {sender,receiver,message}=req.body;
-		const oldData=await databaseProject.chat.findOne({'userIDs':[sender,receiver]});
-		await databaseProject.chat.updateOne({'userIDs':[sender,receiver]},{$set: {message:[...oldData.message,message]}})
+		const { sender, receiver, message } = req.body;
+		const oldData = await databaseProject.chat.findOne({ 'userIDs': [sender, receiver] });
+		await databaseProject.chat.updateOne({ 'userIDs': [sender, receiver] }, { $set: { message: [...oldData.message, message] } })
 		return res.json({
-			payload:{},
-			message:"success",
-			success:true
+			payload: {},
+			message: "success",
+			success: true
 		})
 	} catch (error) {
 		return next(error)
