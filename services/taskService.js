1c0946c863a90722ef57eac2303492330e344d82
vwaizer <lightwing2208@gmail.com>
2024-11-26 20:04:00 +0700

    feature: add aggregate getAllTask

--- services/taskService.js
+++ services/taskService.js
@@ -3,11 +3,40 @@ import databaseProject from "../mongodb.js";
 
 export const getAllTask = async (req, res, next) => {
   const userID = req.userID;
-  const search=req.query?.search;
+
+  const search = decodeURI(req.query?.search);
   try {
-    const payload = await databaseProject.task
-      .find({ registeredMembers: { $in: [userID] },fullName: { $regex: search || "", $options: "i" } })
-      .toArray();
+    // const payload = await databaseProject.task
+    //   .find({ registeredMembers: { $in: [userID] },title: { $regex: search || "", $options: "i" } })
+    //   .toArray();
+    const payload = await databaseProject.task.aggregate([
+      {
+        '$addFields': {
+          'lookupField': {
+            '$toObjectId': '$projectID'
+          }
+        }
+      }, {
+        '$lookup': {
+          'from': 'project',
+          'localField': 'lookupField',
+          'foreignField': '_id',
+          'as': 'result'
+        }
+      }, {
+        '$match': {
+          'registeredMembers': [userID],
+          $or: [
+            { title: { $regex: search, $options: "i" } },
+            { code: { $regex: search, $options: "i" } }
+          ]
+        }
+      }, {
+        '$sort': {
+          'endDate': -1
+        }
+      }
+    ]).toArray()
     return res.json({
       payload: payload,
       success: true,
@@ -49,18 +78,18 @@ export const updateTask = async (req, res, next) => {
     return next(error);
   }
 };
-export const deleteTask= async(req,res,next)=>{
+export const deleteTask = async (req, res, next) => {
   const projectID = req.query?.projectID;
   const taskID = req.params.id;
   try {
-   const oldData= await databaseProject.project.findOne({_id:new ObjectId(projectID)})[0];
-   const newTasks=oldData.taskIDs.filters((item)=> item !== taskID);
-   await databaseProject.project.updateOne({_id:new ObjectId(projectID)},{$set:{'taskIDs':newTasks}});
-   return res.json({
-    payload: {},
-    success: true,
-    message: "Success",
-  });
+    const oldData = await databaseProject.project.findOne({ _id: new ObjectId(projectID) })[0];
+    const newTasks = oldData.taskIDs.filters((item) => item !== taskID);
+    await databaseProject.project.updateOne({ _id: new ObjectId(projectID) }, { $set: { 'taskIDs': newTasks } });
+    return res.json({
+      payload: {},
+      success: true,
+      message: "Success",
+    });
   } catch (error) {
     return next(error);
   }
