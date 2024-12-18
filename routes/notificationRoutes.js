e0c7b47807a0d7a63fcf5db4c561726305ed9a57
vwaizer <lightwing2208@gmail.com>
2024-12-18 17:44:43 +0700

    fix: http type

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -3,6 +3,6 @@ import { userValidator } from '../middleware/validator/userValidator.js';
 import { checkTasksStatusOverdue, getAllNotifications, readAllNotifications } from '../services/notificationService.js';
 
 export const notificationRouter = express.Router();
-notificationRouter.post('/check', userValidator, checkTasksStatusOverdue);
+notificationRouter.get('/check', userValidator, checkTasksStatusOverdue);
 notificationRouter.post('/read', userValidator, readAllNotifications);
 notificationRouter.get('/my', userValidator, getAllNotifications);
