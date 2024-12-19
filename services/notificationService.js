9f754ddf77a17bb2c33c41eb0ee91ec793b7a727
vwaizer <lightwing2208@gmail.com>
2024-12-19 20:36:50 +0700

    fix: add check to /my

--- services/notificationService.js
+++ services/notificationService.js
@@ -24,7 +24,7 @@ export const sendNotification = async (recipientIds, type, authorId, targetId) =
     return next(error);
   }
 };
-export const checkTasksStatusOverdue = async (req, res, next) => {
+export const checkTasksStatusOverdue = async (req) => {
   const readerId = req.userId;
   try {
     const result = await databaseProject.task.aggregate([
@@ -39,24 +39,26 @@ export const checkTasksStatusOverdue = async (req, res, next) => {
           endDate: -1,
         },
       },
-    ]).toArray();    
-    
-    const payload = result.filter((material) => new Date().getTime() - new Date(material.endDate).getTime() <= 604800000 && new Date().getTime() - new Date(material.endDate).getTime() >= 0 ).map((item) => new Notification({
+    ]).toArray();
+
+    const payload = result.filter((material) => new Date().getTime() - new Date(material.endDate).getTime() <= 604800000 && new Date().getTime() - new Date(material.endDate).getTime() >= 0).map((item) => new Notification({
       recipientId: new ObjectId(readerId),
       type: NotificationType.TASK_OVERDUE,
       authorId: new ObjectId(item.leaderId),
       targetId: new ObjectId(item._id)
-    }))    
-    const payloadId=payload.map((item)=> item.targetId);
-    const checkResult= (await databaseProject.notification.find({targetId: {$in:payloadId}}).toArray()).map((item)=> item.targetId.toString());
-    const notExistId= payload.filter((item)=> !checkResult.includes(item.targetId.toString()));
+    }))
+    const payloadId = payload.map((item) => item.targetId);
+    const checkResult = (await databaseProject.notification.find({ targetId: { $in: payloadId } }).toArray()).map((item) => item.targetId.toString());
+    const notExistId = payload.filter((item) => !checkResult.includes(item.targetId.toString()));
 
     if (notExistId.length !== 0) {
       await databaseProject.notification.insertMany(notExistId);
     }
-    return res.json({ payload: {}, success: true, message: 'check tasks status OVERDUE : success' });
+    // return res.json({ payload: {}, success: true, message: 'check tasks status OVERDUE : success' });
+    return { success: true, error: {} };
   } catch (error) {
-    return next(error);
+    // return next(error);
+    return { success: false, error };
   }
 };
 export const readAllNotifications = async (req, res, next) => {
@@ -66,7 +68,7 @@ export const readAllNotifications = async (req, res, next) => {
       { isRead: false, recipientId: new ObjectId(readerId) },
       { $set: { isRead: true } },
     );
-    return res.json({ payload: {}, success: true, message: 'Read  all notifications success' });
+    return res.json({ payload: {}, success: true, message: 'Read  all notifications success' })
   } catch (error) {
     return next(error);
   }
@@ -74,57 +76,61 @@ export const readAllNotifications = async (req, res, next) => {
 
 export const getAllNotifications = async (req, res, next) => {
   const recipientId = req.userId;
-
   try {
-    const notifications = await databaseProject.notification
-      .aggregate([
-        {
-          $match: { recipientId: new ObjectId(recipientId) },
-        },
-        {
-          $lookup: {
-            from: 'user',
-            localField: 'authorId',
-            foreignField: '_id',
-            as: 'author',
+    const checkStatus = await checkTasksStatusOverdue(req);
+    if (checkStatus.success) {
+      const notifications = await databaseProject.notification
+        .aggregate([
+          {
+            $match: { recipientId: new ObjectId(recipientId) },
           },
-        },
-        { $unwind: '$author' },
-        { $unset: 'authorId' },
-        { $sort: { createdAt: -1 } },
-      ])
-      .toArray();
+          {
+            $lookup: {
+              from: 'user',
+              localField: 'authorId',
+              foreignField: '_id',
+              as: 'author',
+            },
+          },
+          { $unwind: '$author' },
+          { $unset: 'authorId' },
+          { $sort: { createdAt: -1 } },
+        ])
+        .toArray();
 
-    // console.log(notifications);
+      // console.log(notifications);
 
-    const targetPopulatedNotifications = await Promise.all(
-      notifications.map(async (notification) => {
-        switch (notification.type) {
-          case NotificationType.TASK_UPDATE:
-          case NotificationType.TASK_COMMENT:
-          case NotificationType.TASK_ASSIGN:
-          case NotificationType.TASK_UNASSIGN: {
-            const task = await databaseProject.task.findOne({
-              _id: notification.targetId,
-            });
-            if (notification.type === NotificationType.TASK_UNASSIGN) {
-              console.log(notification, task);
+      const targetPopulatedNotifications = await Promise.all(
+        notifications.map(async (notification) => {
+          switch (notification.type) {
+            case NotificationType.TASK_UPDATE:
+            case NotificationType.TASK_COMMENT:
+            case NotificationType.TASK_ASSIGN:
+            case NotificationType.TASK_UNASSIGN: {
+              const task = await databaseProject.task.findOne({
+                _id: notification.targetId,
+              });
+              if (notification.type === NotificationType.TASK_UNASSIGN) {
+                console.log(notification, task);
+              }
+              return { ...notification, target: task };
             }
-            return { ...notification, target: task };
-          }
 
-          case NotificationType.PROJECT_INVITE:
-          case NotificationType.PROJECT_REMOVE_MEMBER: {
-            const project = await databaseProject.project.findOne({
-              _id: notification.targetId,
-            });
-            return { ...notification, target: project };
+            case NotificationType.PROJECT_INVITE:
+            case NotificationType.PROJECT_REMOVE_MEMBER: {
+              const project = await databaseProject.project.findOne({
+                _id: notification.targetId,
+              });
+              return { ...notification, target: project };
+            }
           }
-        }
-      }),
-    );
-
-    return res.json({ payload: targetPopulatedNotifications, success: true, message: 'Get all notifications success' });
+        }),
+      );
+      return res.json({ payload: targetPopulatedNotifications, success: true, message: 'Get all notifications success' });
+    }
+    else {
+      return next(checkStatus.error);
+    }
   } catch (error) {
     return next(error);
   }
